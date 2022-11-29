eb96e3056f4f208b6c821e8c974054aa0993503e
NourMakdouli <nour.makdouli@esprit.tn>
2022-11-29 09:43:13 +0100

    department CRUD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -20,7 +22,9 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/model/departement.ts
@@ -0,0 +1,7 @@
+export class    Departement {
+   idDepartement:  number; 
+	nomDepart:string;
+	codeInterne:string; 
+	chefDepart:string;
+	emailDepart:string;  
+}
