89f3e5bef99195e3d094e6c679145efb5d0c987b
MAYMA1 <98560777+MAYMA1@users.noreply.github.com>
2022-12-07 01:25:48 +0100

    ngStyle

--- src/app/universite/list-universite/list-universite.component.ts
+++ src/app/universite/list-universite/list-universite.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Departement } from 'src/app/core/model/departement';
 import { Universite } from 'src/app/core/model/universite';
 import { UniversiteService } from 'src/app/core/services/universite.service';
 
@@ -9,31 +10,62 @@ import { UniversiteService } from 'src/app/core/services/universite.service';
   styleUrls: ['./list-universite.component.scss']
 })
 export class ListUniversiteComponent implements OnInit {
-  searchUni : string ='';
+  searchUni: string = '';
   public list: Universite[];
-  
- // public all: Universite[];
-  constructor(private universiteService: UniversiteService,
+  public unideplist: Departement[];
+  public exist : string;
+
+  // public all: Universite[];
+  constructor(private universiteService: UniversiteService, private currentRoute: ActivatedRoute,
     private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.universiteService.getAllUniversite().subscribe(
-      (response: Universite[]) => { this.list = response ;
-         //filter
-    console.log(this.list)
     
+   
+
+    this.universiteService.getAllUniversite().subscribe(
+      (response: Universite[]) => {
+        this.list = response;
+        //filter
+        console.log(this.list)
+
 
       },
       () => { console.log("error") },
       () => { console.log("complete") },
     );
-  
-     }
-  deleteUni(u:Universite){
+
+    
+
+  }
+  deleteUni(u: Universite) {
     let i = this.list.indexOf(u);
     this.universiteService.delete(u.idUniv).subscribe(
-      ()=>{this.list.splice(i,1)}
+      () => { this.list.splice(i, 1) }
     )
+  }
+
+  getColor(u: Universite) { 
+    this.universiteService.getDeparts(u.idUniv).subscribe(
+      (response: Departement[]) => {
+        this.unideplist = response;
+        if( this.unideplist.length!=0)
+        { this.exist = 'yes';}
+       
+      },
+
+    );
+    switch (this.exist) {
+      case 'yes':
+        return '#dee2e6';
+               default: 
+          return '#ffffff'
+    }     
+     
+
+    
+
+
   }
 
 }
